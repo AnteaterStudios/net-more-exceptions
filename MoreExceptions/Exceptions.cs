f42e55b3e649a818910a466deebc74e791b1567d
Anteater Studios <anteaterstudios.games@gmail.com>
2022-06-24 14:49:13 -0300

    Add one more exception type
    -> StringEmptyOrWhiteSpace

--- MoreExceptions/Exceptions.cs
+++ MoreExceptions/Exceptions.cs
@@ -1,15 +1,13 @@
 ﻿namespace MoreExceptions;
 
 public class EmptyCollectionException : ArgumentException {
-    public EmptyCollectionException()
-    {
-    }
+    public EmptyCollectionException() { }
+    public EmptyCollectionException(string message) : base(message) { }
+    public EmptyCollectionException(string message, string paramName) : base(message, paramName) { }
+}
 
-    public EmptyCollectionException(string message) : base(message)
-    {
-    }
-
-    public EmptyCollectionException(string message, string paramName) : base(message, paramName)
-    {
-    }
+public class StringEmptyOrWhiteSpaceException : ArgumentException {
+    public StringEmptyOrWhiteSpaceException() { }
+    public StringEmptyOrWhiteSpaceException(string message) : base(message) { }
+    public StringEmptyOrWhiteSpaceException(string message, string paramName) : base(message, paramName) { }
 }

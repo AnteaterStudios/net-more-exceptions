4487cb2507ccde7d8f40165a32b327c60a110c30
Anteater Studios <anteaterstudios.games@gmail.com>
2022-06-24 13:40:50 -0300

    Add constructors to EmptyCollectionException class

--- MoreExceptions/Exceptions.cs
+++ MoreExceptions/Exceptions.cs
@@ -1,3 +1,15 @@
 ﻿namespace MoreExceptions;
 
-public class EmptyCollectionException : ArgumentException { }
+public class EmptyCollectionException : ArgumentException {
+    public EmptyCollectionException()
+    {
+    }
+
+    public EmptyCollectionException(string message) : base(message)
+    {
+    }
+
+    public EmptyCollectionException(string message, string paramName) : base(message, paramName)
+    {
+    }
+}

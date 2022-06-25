4207fc56b391b606733dc739b3f05024e43884cb
Anteater Studios <anteaterstudios.games@gmail.com>
2022-06-25 20:32:54 -0300

    Add InvalidTypeParameterException

--- MoreExceptions/Exceptions.cs
+++ MoreExceptions/Exceptions.cs
@@ -11,3 +11,10 @@ public class StringEmptyOrWhiteSpaceException : ArgumentException {
     public StringEmptyOrWhiteSpaceException(string message) : base(message) { }
     public StringEmptyOrWhiteSpaceException(string message, string paramName) : base(message, paramName) { }
 }
+
+public class InvalidTypeParameterException : ArgumentException {
+    public InvalidTypeParameterException() { }
+    public InvalidTypeParameterException(string message) : base(message) { }
+    public InvalidTypeParameterException(string message, string paramName) : base(message, paramName) { }
+    public InvalidTypeParameterException(string message, Type T) : base(message, T.ToString()) { }
+}
